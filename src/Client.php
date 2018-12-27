ce3a34729de099c78c6e66e581014099ca278479
Paweł Feruś <panther.mindc@gmail.com>
2018-12-27 10:21:16 +0100

    changes mime handling

--- src/Client.php
+++ src/Client.php
@@ -6,6 +6,47 @@ use AGSystems\OAuth2\Client\Provider\Allegro;
 use AGSystems\Allegro\REST\Account\Token\AccessTokenInterface;
 use GuzzleHttp\Psr7\Response;
 
+/**
+ * Class Client
+ * @package AGSystems\Allegro\REST
+ *
+ * @method Client offers(int $offerId)
+ * @method Client offer_publication_commands(string $commandId)
+ * @method Client offer_modification_commands(string $commandId)
+ * @method Client offer_price_change_commands(string $commandId)
+ * @method Client offer_quantity_change_commands(string $commandId)
+ * @method Client categories(string $categoryId)
+ * @method Client shipping_rates(string $id)
+ * @method Client offer_contacts(string $id)
+ * @method Client promotions(string $promotionId)
+ *
+ * @property Client sale
+ * @property Client offers
+ * @property Client categories
+ * @property Client parameters
+ * @property Client tasks
+ * @property Client shipping_rates
+ * @property Client delivery_methods
+ * @property Client offer_contacts
+ * @property Client users
+ * @property Client points_of_service
+ * @property Client after_sales_service_conditions
+ * @property Client return_policies
+ * @property Client implied_warranties
+ * @property Client warranties
+ * @property Client pricing
+ * @property Client fee_preview
+ * @property Client offer_quotes
+ * @property Client loyalty
+ * @property Client promotions
+ * @property Client listings
+ * @property Client events
+ *
+ * @method mixed get(array $parameters = [], array $requestOptions = [])
+ * @method mixed post(array $parameters = [], array $requestOptions = [])
+ * @method mixed put(array $parameters = [], array $requestOptions = [])
+ * @method mixed delete(array $parameters = [], array $requestOptions = [])
+ */
 class Client extends \AGSystems\REST\AbstractClient
 {
     const RETRIES = 3;
@@ -22,11 +63,14 @@ class Client extends \AGSystems\REST\AbstractClient
 
     public function __construct(
         AccessTokenInterface $accessToken,
-        Allegro $provider
+        Allegro $provider,
+        array $options = []
     )
     {
         $this->accessToken = $accessToken;
         $this->provider = $provider;
+
+        parent::__construct($options);
     }
 
     protected function handlePath($path)
@@ -55,17 +99,20 @@ class Client extends \AGSystems\REST\AbstractClient
         ];
     }
 
-    protected function handlePost($data = [])
+    protected function handlePost($data = null)
     {
         if (!is_array($data) && is_file($data)) {
             return [
                 'base_uri' => 'https://upload.allegro.pl',
                 'headers' => [
-                    'content-type' => getimagesize($data)['mime']
+                    'content-type' => mime_content_type($data)
                 ],
                 'body' => fopen($data, 'r'),
             ];
         }
+
+        if (is_null($data))
+            $data = [];
 
         return parent::handlePost($data);
     }
@@ -85,7 +132,7 @@ class Client extends \AGSystems\REST\AbstractClient
                     'errors' => [(object)['code' => 'ERROR', 'message' => $response->getBody()->getContents()]]
                 ];
             } else {
-                $result = json_decode($response->getBody()->getContents());
+                $result = \GuzzleHttp\json_decode($response->getBody()->getContents());
                 if (isset($result->error))
                     return (object)[
                         'errors' => [(object)['code' => 'ERROR', 'message' => $result->error_description]]
